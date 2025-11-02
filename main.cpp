e6009cf145b5bef360b70b56de29f13ecb645575
cralva <caa081297@gmail.com>
2025-11-02 15:18:21 -0500

    Figured it out

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello CSCI-201!" << std::endl;
+    std::cout << "Hello CSCI 201!" << std::endl;
     std::cout << "My name is" << std::endl;
     return 0;
 }
