965d304f48ce52ee742274101e4691814cb4cd0f
cralva <caa081297@gmail.com>
2025-11-02 15:42:49 -0500

    made person class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
+#include <string>
+
+class Person {
+
+
+private: //marks everything below as private
+    std::string name; //this and age are going to be private. everything by default are private
+    int age;
+
+public: //now everything is going to be public. the private ended
+    Person(std::string name, int age);
+    std::string getName();
+    void setName(std::string name);
+    int getAge();
+    void setAge(int age);
+    void greet();
+};
+
 
 int main() {
     std::cout << "Hello CSCI 201!" << std::endl;
